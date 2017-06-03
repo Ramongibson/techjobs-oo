ac2c67801795a202fdac0a1d3adb4ee86ea69eba
Ramon Gibson <Ramongibson23@gmail.com>
2017-06-03 08:53:54 -0500

    completed steps 1-5

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -21,6 +21,10 @@ public class JobForm {
 
     @NotNull
     private int employerId;
+    private int locationId;
+    private int coreCompetencieId;
+    private int positionTypeId;
+
 
     /*
         TODO #3 - Included other fields needed to create a job,
@@ -42,6 +46,9 @@ public class JobForm {
         */
 
         employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
 
     }
 
@@ -91,5 +98,29 @@ public class JobForm {
 
     public void setPositionTypes(ArrayList<PositionType> positionTypes) {
         this.positionTypes = positionTypes;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getCoreCompetencieId() {
+        return coreCompetencieId;
+    }
+
+    public void setCoreCompetencieId(int coreCompetencieId) {
+        this.coreCompetencieId = coreCompetencieId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
     }
 }
